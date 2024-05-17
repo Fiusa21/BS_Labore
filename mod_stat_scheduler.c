04fc96ba6450f548cb1190b4b24154f42c54ec63
Fiusa21 <150459037+Fiusa21@users.noreply.github.com>
2024-05-17 13:56:00 +0200

    Update mod_stat_scheduler.c

--- mod_stat_scheduler.c
+++ mod_stat_scheduler.c
@@ -6,12 +6,13 @@
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Yannick Schilling");
-MODULE_DESCRIPTION("Kernel module for periodic statistics output");
+MODULE_DESCRIPTION("Kernel module for printing latency statistic periodically");
+MODULE_PARM_DESC(delay_ms, "Delay in ms defining the interval of the output");
 
 #define DEFAULT_DELAY_MS 1000
 
 static struct delayed_work mod_work; 
-static int delay_ms = DEFAULT_DELAY_MS; 
+static int delay_ms = DEFAULT_DELAY_MS; // param can be manually adjusted when using insmod 
 
 // function being called
 static void work_handler(struct work_struct *work) {
@@ -19,13 +20,13 @@ static void work_handler(struct work_struct *work) {
     unsigned int i;
 
     // print out stats
-    printk(KERN_INFO "latency_record:\n"); 
+    printk(KERN_INFO "Latency record:\n"); 
     for (i = 0; i < task->latency_record_count; i++) { // latency_record_count in latencytop.h
         printk(KERN_INFO "count: %u, backtrace[0]: %pX\n", task->latency_record[i].count, (void*)task->latency_record[i].backtrace[0]);
     }
 
-    // schedule next work with the assigned delay
-    schedule_delayed_work(&mod_work, msecs_to_jiffies(delay_ms)); // msecs_to_jiffies konvertiert Millisekunden in Jiffies (Zeiteinheit des Kernels)
+    // scedule next work with the assigned delay
+    schedule_delayed_work(&mod_work, msecs_to_jiffies(delay_ms)); // convert jiffies
 }
 
 
@@ -48,4 +49,3 @@ __exit void cleanup_module(void) {
 }
 
 module_param(delay_ms, int, S_IRUGO); // module param, including rights
-MODULE_PARM_DESC(delay_ms, "Delay in milliseconds for statistics output");
