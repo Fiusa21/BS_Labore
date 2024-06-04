cbaf95bfafd9703d3a2f1d33abac10a629883478
Fiusa21 <150459037+Fiusa21@users.noreply.github.com>
2024-06-05 00:38:35 +0200

    Update bench_pipe_sample.c

--- bench_pipe_sample.c
+++ bench_pipe_sample.c
@@ -12,20 +12,20 @@
 #include "bench_utils.h"
 
 #define SLEEP_TIME 1
+#define MEASUREMENTS 10000 // Adjust to a reasonable number for testing
 
-// #define USE_MEMSET
-
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-    const int sizes[] = {64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216};
-    const int sizes_num = sizeof(sizes)/sizeof(sizes[0]);
+    const int sizes[] = {128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288};
+    const int sizes_num = sizeof(sizes) / sizeof(sizes[0]);
     pid_t pid = getpid();
     pid_t pid_child;
     int pipe_fd[2]; // File descriptors for the pipe
     int i;
 
     // Create a pipe
-    if (pipe(pipe_fd) == -1) {
+    if (pipe(pipe_fd) == -1)
+    {
         perror("pipe");
         exit(EXIT_FAILURE);
     }
@@ -35,17 +35,18 @@ int main(int argc, char * argv[])
     if (-1 == ret)
         ERROR("fork", errno);
 
-    if (0 == ret)  {
+    if (0 == ret)
+    {
         /* CHILD PROCESS */
-        char * buffer;
+        char *buffer;
         buffer = malloc(sizes[sizes_num - 1]);
         if (NULL == buffer)
             ERROR("malloc", ENOMEM);
         memset(buffer, 0, sizes[sizes_num - 1]);
         pid_child = getpid();
 
-        // Continuously read data from the pipe until no more data is available
-        while (read(pipe_fd[0], buffer, sizes[sizes_num - 1]) > 0) {
+        while (read(pipe_fd[0], buffer, sizes[sizes_num - 1]) > 0)
+        {
             // Do nothing, just keep reading from the pipe
         }
 
@@ -53,55 +54,73 @@ int main(int argc, char * argv[])
         return (EXIT_SUCCESS);
     }
 
+    int *ticks;
+    ticks = malloc(MEASUREMENTS * sizeof(int));
+    if (NULL == ticks)
+        ERROR("malloc", ENOMEM);
+    memset(ticks, 0, MEASUREMENTS * sizeof(int));
+
     /* PARENT PROCESS: measure the writing into the pipe */
-    for (i = 0; i < sizes_num; i++) {
+    for (i = 0; i < sizes_num; i++)
+    {
         int current_size = sizes[i];
         int j;
+        int min_ticks;
+        int max_ticks;
+        long long ticks_all;
         struct timeval tv_start;
         struct timeval tv_stop;
         double time_delta_sec;
 
         sleep(SLEEP_TIME);
 
-        gettimeofday(&tv_start, NULL);
-        for (j = 0; j < MEASUREMENTS; j++) {
+        for (j = 0; j < MEASUREMENTS; j++)
+        {
+            char *buffer = malloc(current_size);
+            if (buffer == NULL)
+                ERROR("malloc", ENOMEM);
+            memset(buffer, 'a', current_size);
+
             unsigned long long start;
             unsigned long long stop;
+
+            gettimeofday(&tv_start, NULL);
+
             start = getrdtsc();
-#ifdef USE_MEMSET
+            if (write(pipe_fd[1], buffer, current_size) != current_size)
             {
-                char buffer[current_size];
-                memset(buffer, 'a', current_size);
-                // Write data to the pipe
-                if (write(pipe_fd[1], buffer, current_size) == -1) {
-                    perror("write");
-                    exit(EXIT_FAILURE);
-                }
+                perror("write");
+                exit(EXIT_FAILURE);
             }
-#else
-            {
-                char c = 'a';
-                int k;
-                for (k = 0; k < current_size; k++) {
-                    // Write data to the pipe
-                    if (write(pipe_fd[1], &c, 1) != 1) {
-                        perror("write");
-                        exit(EXIT_FAILURE);
-                    }
-                }
-            }
-#endif
             stop = getrdtsc();
+
+            gettimeofday(&tv_stop, NULL);
+
+            ticks[j] = stop - start;
+
+            free(buffer);
         }
-        gettimeofday(&tv_stop, NULL);
 
-        // Calculating the time elapsed
-        time_delta_sec = ((tv_stop.tv_sec - tv_start.tv_sec) + ((tv_stop.tv_usec - tv_start.tv_usec)/(1000.0*1000.0)));
+        min_ticks = INT_MAX;
+        max_ticks = INT_MIN;
+        ticks_all = 0;
+        for (j = 0; j < MEASUREMENTS; j++)
+        {
+            if (min_ticks > ticks[j])
+                min_ticks = ticks[j];
+            if (max_ticks < ticks[j])
+                max_ticks = ticks[j];
+            ticks_all += ticks[j];
+        }
+        ticks_all -= min_ticks;
+        ticks_all -= max_ticks;
 
-        // Output results
-        printf ("PID:%d Time (for %d measurements) for %d Bytes (%.2f MB/s)\n",
-                pid, MEASUREMENTS, current_size,
-                ((double) current_size * MEASUREMENTS)  / (1024.0*1024.0*time_delta_sec) );
+        time_delta_sec = ((tv_stop.tv_sec - tv_start.tv_sec) + ((tv_stop.tv_usec - tv_start.tv_usec) / (1000.0 * 1000.0)));
+
+        printf("PID:%d time: min:%d max:%d Ticks Avg without min/max:%f Ticks (for %d measurements) for %d Bytes (%.2f MB/s)\n",
+               pid, min_ticks, max_ticks,
+               (double)ticks_all / (MEASUREMENTS - 2.0), MEASUREMENTS, current_size,
+               ((double)current_size * MEASUREMENTS) / (1024.0 * 1024.0 * time_delta_sec));
     }
 
     close(pipe_fd[0]); // Close the read end of the pipe in the parent process
