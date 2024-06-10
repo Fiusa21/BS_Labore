ae2edb0cb96fc7dac30b990e8db36f1a1b619c54
Fiusa21 <150459037+Fiusa21@users.noreply.github.com>
2024-06-10 16:16:16 +0200

    Update bench_pipe_sample.c

--- bench_pipe_sample.c
+++ bench_pipe_sample.c
@@ -1,100 +1,76 @@
-#define _GNU_SOURCE
+/*
+ * Small benchmark of Anonymous Memory Mmaps using Pipes for IPC.
+ *
+ * Author: Rainer Keller, HS Esslingen
+ * Modified by: ChatGPT
+ */
+
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <string.h>
 #include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-#include <fcntl.h>
-#include <limits.h>
+#include <sys/mman.h>
+#include <err.h>
 #include <errno.h>
-#include <assert.h>
+#include <fcntl.h>
+#include <string.h>
+#include <limits.h>
+#include <sys/time.h>
 
 #include "bench_utils.h"
 
+#define SLEEP_TIME 1
 
+// #define USE_MEMSET
 
-int main(int argc, char * argv[]) {
-    const int sizes[] = {64, 256, 1024, 4096, 16384, 65536, 262144, 1048576, 4194304, 16777216};
-    const int sizes_num = sizeof(sizes)/sizeof(sizes[0]);
+int main(int argc, char * argv[])
+{
+    const int sizes[] = {128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288};
+    const int sizes_num = sizeof(sizes) / sizeof(sizes[0]);
 #define MAX_SIZE sizes[sizes_num-1]
-    int pipe_child_to_parent[2];
-    int pipe_parent_to_child[2];
-    char * buffer;
-    pid_t pid;
+    pid_t pid = getpid();
     pid_t pid_child;
-    int ret;
-
-    // Open pipes to communicate the pointer to buffer
-    ret = pipe(pipe_child_to_parent);
-    if (-1 == ret)
-        ERROR("pipe child_to_parent", errno);
-    ret = pipe(pipe_parent_to_child);
-    if (-1 == ret)
-        ERROR("pipe parent_to_child", errno);
-
-    pid = getpid();
-    ret = pid_child = fork();
-    if (-1 == ret)
-        ERROR("fork", errno);
-
-    if (0 == ret) {
-        /* CHILD Process */
-        int num_read;
-        int to_read;
-        pid = getpid();
-        DEBUG(printf("PID:%d (CHILD) starts\n", (int) pid));
-
-        // Close the reading end in child_to_parent and writing end in parent_to_child
-        ret = close(pipe_child_to_parent[0]);
-        if (ret == -1) ERROR("close", errno);
-        ret = close(pipe_parent_to_child[1]);
-        if (ret == -1) ERROR("close", errno);
-
-        buffer = malloc(MAX_SIZE);
-        if (NULL == buffer)
-            ERROR("malloc", ENOMEM);
-        memset(buffer, 0, MAX_SIZE);
-
-        for (int i = 0; i < sizes_num; i++) {
-            int current_size = sizes[i];
-            to_read = current_size;
-            for (num_read = 0; num_read < current_size; num_read += ret, to_read -= ret) {
-                ret = read(pipe_parent_to_child[0], buffer + num_read, to_read);
-                if (ret == -1) ERROR("read", errno);
-            }
-        }
-
-        DEBUG(printf("PID:%d (CHILD) exits\n", (int) pid));
-        free(buffer);
-        exit(EXIT_SUCCESS);
-    }
-
-    int num_written;
-    int to_write;
-    int * ticks;
     int i;
 
-    DEBUG(printf("PID:%d (PARENT) starts child_pid:%d\n", (int) pid, (int)pid_child));
+    int pipefd[2];
+    if (pipe(pipefd) == -1) {
+        ERROR("pipe", errno);
+    }
 
-    // Close the reading end in parent_to_child and writing end in child_to_parent
-    ret = close(pipe_parent_to_child[0]);
-    if (ret == -1) ERROR("close", errno);
-    ret = close(pipe_child_to_parent[1]);
-    if (ret == -1) ERROR("close", errno);
+    int ret = pid_child = fork();
 
-    buffer = malloc(MAX_SIZE);
-    if (NULL == buffer)
-        ERROR("malloc", ENOMEM);
-    memset(buffer, 'a', MAX_SIZE);  // Fill the buffer with 'a's
+    if (-1 == ret) {
+        ERROR("fork", errno);
+    }
 
+    if (0 == ret)  {
+        /* CHILD */
+        char *buffer;
+        buffer = malloc(MAX_SIZE);
+        if (NULL == buffer) {
+            ERROR("malloc", ENOMEM);
+        }
+        memset(buffer, 0, MAX_SIZE);
+        pid_child = getpid();
+
+        close(pipefd[0]);  // Close the read end of the pipe
+        write(pipefd[1], buffer, MAX_SIZE);
+        close(pipefd[1]);  // Close the write end of the pipe after writing
+        sleep(SLEEP_TIME);
+        printf("PID %d (CHILD): COPY DONE\n", pid_child);
+        free(buffer);
+        return (EXIT_SUCCESS);
+    }
+    
+    int *ticks;
     ticks = malloc(MEASUREMENTS * sizeof(int));
-    if (NULL == ticks)
+    if (NULL == ticks) {
         ERROR("malloc", ENOMEM);
-    memset(ticks, 0, MEASUREMENTS * sizeof(int));
+    }
+    memset(ticks, 0, MEASUREMENTS * sizeof(int));    
 
+    /* PARENT: measure the writing into the buffer */
     for (i = 0; i < sizes_num; i++) {
         int current_size = sizes[i];
         int j;
@@ -105,23 +81,40 @@ int main(int argc, char * argv[]) {
         struct timeval tv_stop;
         double time_delta_sec;
 
-        assert(current_size <= MAX_SIZE);
-
+        sleep(SLEEP_TIME);
+        
         gettimeofday(&tv_start, NULL);
         for (j = 0; j < MEASUREMENTS; j++) {
             unsigned long long start;
             unsigned long long stop;
-            start = getrdtsc();
-            to_write = current_size;
-            for (num_written = 0; num_written < current_size; num_written += ret, to_write -= ret) {
-                ret = write(pipe_parent_to_child[1], buffer + num_written, to_write);
-                if (ret == -1) ERROR("write", errno);
+
+            char *anon = malloc(current_size);
+            if (anon == NULL) {
+                ERROR("malloc", ENOMEM);
             }
+
+            start = getrdtsc();
+#ifdef USE_MEMSET
+            memset(anon, 'a', current_size);
+#else
+            {
+                int k;
+                for (k = 0; k < current_size; k++) {
+                    anon[k] = 'a';
+                }
+            }
+#endif
             stop = getrdtsc();
             ticks[j] = stop - start;
+
+            close(pipefd[1]);  // Close the write end of the pipe
+            read(pipefd[0], anon, current_size);
+            close(pipefd[0]);  // Close the read end of the pipe after reading
+
+            free(anon);
         }
         gettimeofday(&tv_stop, NULL);
-
+            
         min_ticks = INT_MAX;
         max_ticks = INT_MIN;
         ticks_all = 0;
@@ -135,17 +128,12 @@ int main(int argc, char * argv[]) {
 
         time_delta_sec = ((tv_stop.tv_sec - tv_start.tv_sec) + ((tv_stop.tv_usec - tv_start.tv_usec) / (1000.0 * 1000.0)));
 
-        printf("PID:%d, Size:%d, Avg. Ticks: %f (for %d measurements), Time: %.3fs, Speed: (%.2f MB/s)\n",
-               pid, current_size,
-               (double) ticks_all / (MEASUREMENTS - 2.0), MEASUREMENTS, time_delta_sec,
+        printf("PID:%d time: min:%d max:%d Ticks Avg without min/max:%f Ticks (for %d measurements) for %d Bytes (%.2f MB/s)\n",
+               pid, min_ticks, max_ticks,
+               (double) ticks_all / (MEASUREMENTS - 2.0), MEASUREMENTS, current_size,
                ((double) current_size * MEASUREMENTS) / (1024.0 * 1024.0 * time_delta_sec));
     }
 
-    // Tell Child to exit:
-    write(pipe_parent_to_child[1], &pid, 1);
-
-    free(buffer);
     free(ticks);
-
-    return EXIT_SUCCESS;
+    return (EXIT_SUCCESS);
 }
